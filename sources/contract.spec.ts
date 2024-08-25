13081d3ae0880ae3296e6cd2242fdb08828d8343
Evgenii Akentev <i@ak3n.com>
2024-08-25 16:12:34 +0400

    Add declareDocument test

--- sources/contract.spec.ts
+++ sources/contract.spec.ts
@@ -1,7 +1,8 @@
-import { toNano, beginCell } from "@ton/core";
-import { Blockchain } from "@ton/sandbox";
-import "@ton/test-utils";
-import { Autoproof } from "./output/autoproof_Autoproof";
+import { toNano, beginCell, Address } from "@ton/core"
+import { Blockchain } from "@ton/sandbox"
+import "@ton/test-utils"
+import { Autoproof } from "./output/autoproof_Autoproof"
+import { Document } from "./output/autoproof_Document"
 
 describe("contract", () => {
     it("should deploy correctly", async () => {
@@ -48,6 +49,41 @@ describe("contract", () => {
       const comms = await contract.getCurrentCommissions()
       expect(comms!.transfershipCommissionPercentage).toEqual(90n)
       expect(comms!.royaltyCommissionPercentage).toEqual(10n)
+    })
+
+
+    it("should deploy document contract", async () => {
+        let system = await Blockchain.create();                                                                        
+        let owner = await system.treasury("owner");
+        let contract = system.openContract(await Autoproof.fromInit());
+        await contract.send(owner.getSender(), { value: toNano(1), bounce: false }, { $$type: "Deploy", queryId: 0n });
+        
+      await contract.send(owner.getSender(), { value: toNano(1), bounce: false }, "Resume")
+
+      const declResult = await contract.send(owner.getSender(), { value: toNano(1) },
+        { $$type: "DeclareDocument",
+          royaltyWalletAddress: null,
+          document:
+            { $$type: "DocumentData",
+              author: {
+                $$type: "PersonDetails",
+                name: "name",
+                address: "address"
+              },
+              title: "document",
+              rootHash: "hash",
+              data: "data",
+              tags: null,
+              description: null
+            },
+         })
+
+      const documentContractAddress = declResult.transactions[1].outMessages.values()[0].info.dest
+
+      let dc = system.openContract(Document.fromAddress(documentContractAddress as Address))
+
+     const documentAuthor = await dc.getAuthor()
+     expect(documentAuthor.toString()).toEqual(owner.address.toString())
     })
 
 })
